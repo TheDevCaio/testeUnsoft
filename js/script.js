fdccc407a575bd7db3bf6b1550caa19a4dea6f61
Caio Dev <caioinnocencio@gmail.com>
2025-08-11 09:10:25 -0300

    Edição final no leyalt da tela.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,17 @@
 $(document).ready(function(){
-    $('.carousel-super').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        responsive: [
-            { breakpoint: 992, settings: { slidesToShow: 2 } },
-            { breakpoint: 576, settings: { slidesToShow: 1 } }
-        ]
-    });
-
-    $('.carousel-venda').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        responsive: [
-            { breakpoint: 992, settings: { slidesToShow: 2 } },
-            { breakpoint: 576, settings: { slidesToShow: 1 } }
-        ]
-    });
-
-    $('.carousel-locacao').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        responsive: [
-            { breakpoint: 992, settings: { slidesToShow: 2 } },
-            { breakpoint: 576, settings: { slidesToShow: 1 } }
-        ]
-    });
+  $('.carousel-super, .carousel-venda, .carousel-locacao').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: true,  // ativa as setas
+    prevArrow: '<button type="button" class="slick-prev"><i class="bi bi-arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="bi bi-arrow-right"></i></button>',
+    responsive: [
+      { breakpoint: 992, settings: { slidesToShow: 2 } },
+      { breakpoint: 576, settings: { slidesToShow: 1 } }
+    ]
+  });
 });
